2f844974044cf5219d821225fd8033107db5fb62
ANDROPOLISS <andropoliss@gmail.com>
2015-01-14 23:00:11 +0200

    add AnanlizorBase class

---
+++ Analizers/AnalizerBase.java
@@ -0,0 +1,19 @@
+package Analizers;
+
+import net.n3.nanoxml.IXMLElement;
+import net.n3.nanoxml.XMLElement;
+
+public abstract class AnalizerBase { // base class for analizators
+	protected XMLElement xml;
+	protected abstract void Analize(); // main analization method
+	public AnalizerBase(XMLElement _xml) {
+		this.xml = _xml;
+	}
+	public XMLElement getResult() {
+		return this.xml;
+	}
+	public void setXmlElement(XMLElement _xml) {
+		this.xml = _xml;
+	}
+	
+}
